295332b121243adb7d7edf89a30febc43692c060
TanjimMotin <tanjimotin21@gmail.com>
2018-10-08 15:38:00 -0400

    class addes

--- index.js
+++ index.js
@@ -1,56 +1,160 @@
-$(document).ready(()=>{
-	var container = document.getElementById('container')
-	document.getElementById('submit').addEventListener('click', function(event){
-		event.preventDefault();
+var array = [152, 172, 182 ]
+var counter = 0;
+var left = document.getElementById('left')
+var right = document.getElementById('right')
+var mainImage = document.querySelector(".main-image")
 
-		var title = document.getElementById('title').value
-$.ajax({url:"http://www.omdbapi.com/?apikey=71f636f9&",
-        data:{s: 'titanic'},
-        					success: function(response){
-						var a = response.Search[0]
-						console.log(a)
-					    $("#text1").html(a.Title);
-					    $("#text3").html(a.Director); 
-					    $("#actors").html(a.Actors) 
-					    $("#year").html(a.Year)
-					    $("#plot").html(a.Plot)
-						$("#text2").attr("src", a.Poster);
-						for(var i=0;i<response.Search.length;i++){ 
-							searchResults(response.Search[i])
-						}
-						
-					}
-		})
-	})									
-									
-	function searchResults(movie){
-		var resultDiv = "<div class='result'><img src='" + movie.Poster + "'>Title: " + movie.Title + "</div>";
-		container.innerHTML += resultDiv;
+
+
+class Pokemon{
+	constructor(url, name){
+		this.url = url;
+		this.name = name;
 	}
+}
+
+class Trainer {
+
+	all(){
+
+	}
+}
 
 
+	$.ajax({url: " https://fizal.me/pokeapi/api/" + array[0] + ".json",
+		data: {},
+		dataType: "json",
+		success:function(response){
+			builtClass(response)
+
+			document.querySelector(".hp").innerHTML = response.stats[5].base_stat
+			document.querySelector(".attack").innerHTML = response.stats[4].base_stat
+			document.querySelector(".defence").innerHTML =  response.stats[3].base_stat
+		}
+	})
+
+right.addEventListener('click', function(){	
+
+	counter += 1;
+	if(counter > 2){
+		counter = 0;
+	}
+	$.ajax({url: " https://fizal.me/pokeapi/api/" + array[counter] + ".json",
+		data: {},
+		dataType: "json",
+		success:function(response){
+			builtClass(response)
+
+			document.querySelector(".hp").innerHTML = response.stats[5].base_stat
+			document.querySelector(".attack").innerHTML = response.stats[4].base_stat
+			document.querySelector(".defence").innerHTML = response.stats[3].base_stat
+		}
+	})
 })
-        /*success: function(response){
-        	hookImage(response)
 
+left.addEventListener('click', function(){	
 
-        }
+	counter -= 1;
+	if(counter < 0){
+		counter = array.length-1;
+	}
+	$.ajax({url: " https://fizal.me/pokeapi/api/" + array[counter] + ".json",
+		data: {},
+		dataType: "json",
+		success:function(response){
+			builtClass(response)
 
-
+			document.querySelector(".hp").innerHTML = response.stats[5].base_stat
+			document.querySelector(".attack").innerHTML = response.stats[4].base_stat
+			document.querySelector(".defence").innerHTML = response.stats[3].base_stat
+		}
+	})
 })
 
 
-function hookImage(x){
-   img.style.backgroundImage = "url(" + x.Poster + ")"
-   img.style.backgroundSize = "100% 100%";
+
+function builtClass(x){
+	a = new Pokemon(x.sprites.front_default, x.name)
+	format(a)
+}
 
 
-}	*/
+function format(y){
+			mainImage.style.backgroundImage = "url(" + y.url+ ")"
+			mainImage.style.backgroundSize = "100% 100%"
+			mainImage.style.border = '2px solid';
+			mainImage.style.width = '300px'
+			mainImage.style.height = '300px'
+			mainImage.style.margin = '20px auto'
+			document.querySelector(".name").innerHTML = y.name;
 
-	
+}
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var image = document.getElementById('image')
+$.ajax({url:"https://fizal.me/pokeapi/api/152.json",
+        success: function (response){
+          console.log(response)
+          image.style.backgroundImage = "url(" + response.sprites.front_default + ")"
+
+
+                                    }
+       })
+       
+$.ajax({url:"https://fizal.me/pokeapi/api/172.json",
+        success: function (response2){
+          console.log(response2)
+          image2.style.backgroundImage = "url(" + response2.sprites.front_default + ")"
+
+                                     }
+       })
+
+$.ajax({url:"https://fizal.me/pokeapi/api/182.json",
+        success: function (response3){
+          console.log(response3)
+          image3.style.backgroundImage = "url(" + response3.sprites.front_default + ")"
+
+                                     }
+      })*/
